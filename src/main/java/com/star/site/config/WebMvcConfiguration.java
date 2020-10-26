12a3669fc5bfbafdf352737bdc6238339783f96c
star <irelia@star0224.com>
2020-10-26 14:21:44 +0800

    处理 fast-json 会出现 `Content-Type cannot contain wildcard type '*'` 的问题

--- src/main/java/com/star/site/config/WebMvcConfiguration.java
+++ src/main/java/com/star/site/config/WebMvcConfiguration.java
@@ -4,17 +4,19 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
-    // 允许CORS
+    // 允许 CORS
     @Override
     protected void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -29,6 +31,28 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         super.configureMessageConverters(converters);
         // 使用FastJson作为默认的MessageConverter，将接受参数(JSON类型)转为对象
         FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+
+        // 处理 fast-json 会出现 `Content-Type cannot contain wildcard type '*'` 的问题
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+//        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+        supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+        supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+        supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XML);
+        supportedMediaTypes.add(MediaType.IMAGE_GIF);
+        supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+        supportedMediaTypes.add(MediaType.IMAGE_PNG);
+        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+        supportedMediaTypes.add(MediaType.TEXT_HTML);
+        supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+        supportedMediaTypes.add(MediaType.TEXT_XML);
+        fastConverter.setSupportedMediaTypes(supportedMediaTypes);
+
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(
                 SerializerFeature.PrettyFormat
